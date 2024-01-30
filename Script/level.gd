3be56427fa521b9013e75f962243bf7a5d2b9ffc
Rasya Devansyah <rasya.devan21@gmail.com>
2024-01-30 16:09:12 +0700

    Initial Commit

---
+++ Script/level.gd
@@ -0,0 +1,48 @@
+extends Node2D
+
+var isPressDown = false
+var start_mouse_position = Vector2.ZERO
+
+var rigid_body: RigidBody2D
+
+func _ready():
+	rigid_body = $RigidBody2D
+
+func _process(delta):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) && isPressDown == false:
+		isPressDown = true
+		start_mouse_position = get_global_mouse_position()
+		
+	elif !Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) && start_mouse_position != Vector2.ZERO && isPressDown == true:
+		isPressDown = false
+		var end_mouse_position = get_global_mouse_position()
+		# Print the coordinates when pressed and released
+		print("Mouse Pressed Coordinates: ", start_mouse_position)
+		print("Mouse Released Coordinates: ", end_mouse_position)
+		
+		AddForceToBall(start_mouse_position, end_mouse_position)
+		
+
+
+func AddForceToBall(start_mouse_position, end_mouse_position):
+		# Calculate distance and direction
+		var distance = start_mouse_position.distance_to(end_mouse_position)
+		var direction = -(end_mouse_position - start_mouse_position).normalized()
+
+		# Print distance and direction
+		print("Distance: ", distance)
+		print("Direction: ", direction)
+		
+		#remove any forces to 0
+		rigid_body.linear_velocity = Vector2.ZERO
+		rigid_body.angular_velocity = 0.0
+		
+		# Add force to the rigid body
+		var force_strength =  distance * 20# Adjust the force strength as needed
+		if force_strength > 1500:
+			force_strength = 1500
+	
+		rigid_body.apply_central_impulse(direction * force_strength)
+
+		# Reset start position for the next press/release cycle
+		start_mouse_position = Vector2.ZERO
